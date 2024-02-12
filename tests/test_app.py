f36eadbc2f733970b6317a0964cc4f18cd346592
Ahmed Baig <ahmedbaig137@gmail.com>
2024-02-12 10:17:12 +0500

    added test code

---
+++ tests/test_app.py
@@ -0,0 +1,15 @@
+# tests/test_app.py
+from app import app
+
+import pytest
+
+@pytest.fixture
+def client():
+    app.config['TESTING'] = True
+    with app.test_client() as client:
+        yield client
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
+    assert b"Welcome to Flask App" in response.data
